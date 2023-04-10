aa2eed629c40eb687389387e7edc8aabe1139d92
91anita <45237162+91anita@users.noreply.github.com>
2023-04-11 00:07:21 +0530

    collegename added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import AddUser from './Components/Users/AddUser';
 import UsersList from './Components/Users/UsersList';
@@ -6,11 +6,11 @@ import UsersList from './Components/Users/UsersList';
 function App() {
   const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
+  const addUserHandler = (uName, uAge,cName) => {
     setUsersList((prevUsersList) => {
       return [
         ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
+        { name: uName, age: uAge, id: Math.random().toString(), college: cName},
       ];
     });
   };
